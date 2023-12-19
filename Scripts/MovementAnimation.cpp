e292df2994a56fdb372f3fe0a1fd81c3c85c7fd9
Olaf van den Berg <oa.vandenberg@student.avans.nl>
2023-12-19 11:22:48 +0100

    fix player animating while not moving

--- Scripts/MovementAnimation.cpp
+++ Scripts/MovementAnimation.cpp
@@ -16,7 +16,7 @@ void MovementAnimation::onUpdate(milliseconds deltaTime) {
     auto &animation = tryGetComponent<AnimationComponent>();
     auto &sprite = tryGetComponent<SpriteComponent>();
 
-    if (velocity.velocity.getX() != 0 || velocity.velocity.getY() != 0) {
+    if (velocity.velocity != Vector2(0, 0)) {
         if (velocity.velocity.getX() > 0) {
             sprite.flipX = false;
         } else if (velocity.velocity.getX() < 0) {
@@ -24,7 +24,7 @@ void MovementAnimation::onUpdate(milliseconds deltaTime) {
         }
         animation.isPlaying = true;
     } else {
-//        animation.isPlaying = false;
+        animation.isPlaying = false;
         sprite.tileOffset = std::make_unique<Vector2>(0, 0);
 
     }
