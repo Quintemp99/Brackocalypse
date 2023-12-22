bede89f16175007e7a17cc0c1c35af7b214d2164
Stef <stefvanstipdonk@gmail.com>
2023-12-22 11:17:32 +0100

    Try catch added

--- Src/Scripts/EnemyFollowPlayer.cpp
+++ Src/Scripts/EnemyFollowPlayer.cpp
@@ -6,23 +6,29 @@
 #include <Components/AIComponent.hpp>
 #include "EnemyFollowPlayer.hpp"
 
+#include "Helpers/UserLogger.hpp"
+
 
 void EnemyFollowPlayer::onStart() {
-    auto& aiComponent = tryGetComponent<AIComponent>();
-    auto graphGameobject = getGameObjectByName(graphName);
-    if(!graphGameobject.has_value()) return;
-    aiComponent.graphId = graphGameobject.value()->getEntityId();
+    try {
+        auto &aiComponent = tryGetComponent<AIComponent>();
+        auto graphGameobject = getGameObjectByName(graphName);
+        if (!graphGameobject.has_value()) return;
+        aiComponent.graphId = graphGameobject.value()->getEntityId();
+    } catch (std::exception &e) {
+        UserLogger::GetInstance().Error("EnemyFollowPlayer::onStart, crashed " + std::string(e.what()));
+    }
 }
 
 void EnemyFollowPlayer::onUpdate(float deltaTime) {
-    if(isActive){
-        auto& aiComponent = tryGetComponent<AIComponent>();
+    if (isActive) {
+        auto &aiComponent = tryGetComponent<AIComponent>();
 
         auto player = getGameObjectByTag("Player");
         if (!player.has_value()) return;
         auto &playerTransform = player.value().tryGetComponent<TransformComponent>();
 
-        if(aiComponent.target != playerTransform.position){
+        if (aiComponent.target != playerTransform.position) {
             aiComponent.target = std::make_unique<Vector2>(*playerTransform.position);
         }
     }
